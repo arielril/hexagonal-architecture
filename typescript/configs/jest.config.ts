56722a89d945206c6fb6b0825ac82e274ff0b26e
Ariel Ril <ariel.ril@4all.com>
2020-05-06 09:32:10 -0300

    fix tests

--- typescript/configs/jest.config.ts
+++ typescript/configs/jest.config.ts
@@ -1,4 +1,19 @@
+/* eslint-disable no-redeclare */
 import { AnySchema } from '@hapi/joi';
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace jest {
+    // tslint:disable-next-line: interface-name
+    interface Matchers<R, T> {
+      /**
+       * Checks if the object matches the schema
+       * @param schema Joi schema
+       */
+      toMatchSchema(schema: AnySchema): R;
+    }
+  }
+}
 
 /**
  * Checks if the object matches the schema
@@ -18,8 +33,8 @@ function toMatchSchema(received: object, schema: AnySchema) {
   }
 
   return {
-    message: () =>
-      `expected ${JSON.stringify(received)} to be valid, instead the validation return the error: ${JSON.stringify(error.message)}`,
+    message: () => `expected ${
+      JSON.stringify(received)} to be valid, instead the validation return the error: ${JSON.stringify(error.message)}`,
     pass: false,
   };
 }
